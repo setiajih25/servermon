0a4fc3121a5a3c9393a0ccc398d88b285cb6e586
setiajih25 <hendrasetiajih@gmail.com>
2022-10-24 09:28:23 +0700

    modified:   resources/views/dashboard/dashboard_home.blade.php

--- resources/views/dashboard/dashboard_home.blade.php
+++ resources/views/dashboard/dashboard_home.blade.php
@@ -75,7 +75,7 @@
                                                 <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />
                                                 <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
                                             </svg>
-                                        </span>Refresh Data</button>
+                                        </span>Refresh</button>
                                 </div>
                             </div>
                             <div class="table-responsive-md">
